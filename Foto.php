3c8e907c39e479018963b632b4585d96b01ece32
alainvanhoof <alain@lafeberhof.nl>
2020-10-06 17:33:32 +0200

    Delete Foto.php

--- Foto.php
+++
@@ -1,9 +0,0 @@
-<?php
-$PageName="Foto";
-$PageTitle="Lafeberhof Fotoalbum";
-include "header.php";
-?>
-<p>
-De foto's staan op <a href="https://www.lafeberhof.nl/foto/">https://www.lafeberhof.nl/foto</a> (beveiligd met een wachtwoord!).
-</p>
-<?php include "footer.php" ?>
