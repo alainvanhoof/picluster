d4db6887fcb97862cbf400f73d9512065ab64c22
alainvanhoof <alain@lafeberhof.nl>
2020-10-06 17:33:54 +0200

    Delete Home.php

--- Home.php
+++
@@ -1,9 +0,0 @@
-<?php
-$PageName="Home";
-$PageTitle="Welkom op de site van het Lafeberhof";
-include "header.php";
-?>
-<p>
-<img src="luchtfoto.jpg" alt="LuchtFoto">
-</p>
-<?php include "footer.php" ?>
